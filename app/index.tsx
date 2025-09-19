4ad80689a8023f4ee778b0c9df15434b96a2443c
koray kolak <koray_kolak@hotmail.com>
2025-09-19 19:39:31 +0300

    fresh start: reinitialize project and files

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <Text className="text-5xl text-dark-200 font-bold text-center">Welcome to Lectopus!</Text>
+        </View>
+    );
+}
