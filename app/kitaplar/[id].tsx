1ffb3cfd0d00a30d210a67c107f21e718a1a0138
koray kolak <koray_kolak@hotmail.com>
2025-09-23 19:40:14 +0300

    klasörler silindi ve bazı dosyalar güncellendi

---
+++ app/kitaplar/[id].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const KitapDetayları = () => {
+  return (
+    <View>
+      <Text>KitapDetayları </Text>
+    </View>
+  );
+};
+
+export default KitapDetayları;
